730dd7ef077a442948b43c1ba13bbbb28b3c6e87
Jacob Stephens <jacob.stephens.701@gmail.com>
2026-02-12 17:41:04 +0000

    Expand story and make more readable on mobile

---
+++ private/sources/The Story of How We Met - Wedding Website v2.md
@@ -0,0 +1,63 @@
+# Our Story
+
+---
+
+## A Prayer and a Dance
+
+~He came so that we might have life, and have it in abundance.~ John 10:10
+
+In early October of 2024, Jacob moved to Philadelphia, looking for new life and a fresh start. On October 7, he found himself praying using the words of Percy Mayfield, "Please send me someone to love."  
+  
+Two months prior, I had also moved to Philly from the small town of Steubenville, Ohio, feeling called to leave behind the last 7 years of familiar comforts, like weekly tap dancing at a local studio and hosting monthly brunches with friends after church. I felt compelled and spoken to by the Lord through the words of Aly Aleigha, "Into the unexplored enclave, little bird, take flight."
+
+I knew that starting a new life in a new city meant finding a new community. And I knew that I could find community through church, and also through my favorite hobby - dancing. I started visiting different parishes and searching for West Coast Swing, Salsa, and Lindy Hop dance socials, and soon was invited to attend a Blues and Fusion event on October 18th in Old City.
+
+---
+
+## The Sidewalk
+
+A block away from the Liberty Bell, standing outside the Concierge Ballroom and waiting for the doors to open, Jacob and I met. He was tall, handsome, and vivacious - with a kind of golden retriever-energy. The night started with a blues lesson, during which we rotated partners frequently. While I was occupied with learning the style's rhythm and the names of my quickly turning-over partners, Jacob has since admitted that he was stealing glances through the mirror.
+
+At the end of the night, as I gathered my belongings, I caught sight of Jacob across the room out of the corner of my eye. I had this sense that he wanted to talk to me, and curious to see where things might go, I paused by the door to fill out an interest form and give him the chance to approach.
+
+Bingo.
+
+He came over, we chatted a bit more, and then said our goodbyes. After I turned and walked out, he made note of my name from the interest form. The next morning, I saw I had a friend request from Jacob Stephens (sent at 2am the night before).
+
+---
+
+## Rosie the Riveter's Birthday
+
+A week later, I ran into Jacob again - this time at a Lindy Hop social at Cherry St Harbor. We danced several songs over the evening before I ran off to welcome friends from out of town, who had come to celebrate my birthday. Jacob intentionally asked for one last song before I left, so that he could be my last dance on my birthday.
+
+---
+
+## The Orbits Tighten
+
+Another week later, on November 1, the Feast of All Saints, we both found ourselves back at the Concierge Ballroom for a Balboa dance. As we danced and conversed about our days, I mentioned I had gone to Mass for the feast day.
+
+Jacob's ears pricked - he had been feeling drawn to explore the Catholic Church and join a formation class to learn more. At this time I still hadn't settled on a parish community for myself, but had been to St. Agatha St. James a couple of times and thought the church was beautiful and the community faithful. I invited him to join me sometime, hoping I could snag him for the Church and maybe also myself...
+
+The next day, Jacob invited me to join him and a couple other dance friends in seeing live theater - The Obama Musical (which, if you're wondering, was not very good but _was_ entertaining and at times a bit of a fever dream). Afterwards we got tacos, and somehow ended up on the topic of Pope John Paul II's Theology of the Body while he drove me home.
+
+It was at this point that I really started wondering, "Who is this man, and where did he come from? How do I make sure I sound as interesting to him as he does to me?"
+
+---
+
+## Falling
+
+Through November, we texted with some regularity, carpooled to dance events, and even cooked in my kitchen together for a dance group's Friendsgiving dinner. I told him I was going to see Hamilton (much better than The Obama Musical) with friends from church, and from my seat sent him a picture of my view of the stage to tease him about not joining me. Apparently that grainy photo from my cheap Android and his growing desire to leave an impression on me were enough for him to find me in the balcony at intermission with a ticket he bought last minute. As the lights flickered and Act II started, I frantically texted my future maid-of-honor, asking for confirmation of if I was living in a Hallmark movie.
+
+One Sunday, Jacob joined me for Mass, and I noticed him get emotional before I went up to receive Communion. He was recalling his prayer from October 7, and there in the church next to me felt with certainty that God was answering it. Knowing that he could not yet receive the Eucharist as he was still in formation, I pulled up an Act of Spiritual Communion for him. He read it, and the tears started flowing. He later shared that he was deeply moved by the parts of the prayer mentioning *a contrite heart* and *the poor dwelling my heart offers You*. He felt the ache of his old self, and a beckoning towards a newness of life. I myself was grateful for this encounter I could tell he was having with Christ, and wondered what this new path in my life would play out to be.
+
+---
+
+## The First Date
+
+We finally went on our first official date, only a month after meeting but it felt long overdue. After witnessing me make a sausage dish for Friendsgiving, a day after having made the same for another group dinner, he was convinced - "This girl *LOVES sausage" and found a local, vibey spot known for their sausage sandwiches, hoping to impress.
+
+*Note: I like sausage. But the back-to-back dishes were more about the convenience than any preoccupation Jacob assumed I had with the ground pork.
+
+We went on two more dates, I daringly wrote him half of a poem for his half birthday, and then he went radio silent for three days. So when he finally responded by showing up unannounced on my doorstep with two chicken maroosh sandwiches, a massive tub of hummus, and two kinds of baklava, my brain short-circuited. I apologized for my inability to sit and have dinner with him as I was just about to run out for the evening. But later that night we reconnected, walked through the first snow of the year, and after some prodding - I drew out his intentions. He envisioned a long journey ahead for us. 
+
+With wonder and joy, I accepted his invitation.
--- public/story.php
+++ public/story.php
@@ -8,8 +8,19 @@ include __DIR__ . '/includes/header.php';
     <h1 class="page-title">Our Story</h1>
     
     <section class="story-section">
-        <h2>A Dance Begins</h2>
-        <p><em>Jacob and I met fusion dancing in Old City, Philadelphia, and kept meeting at various dance events in the city in October-November of 2024.</em></p>
+        <h2>A Prayer and a Dance</h2>
+        <p class="scripture-quote">He came so that we might have life, and have it in abundance.<span class="reference">&mdash; John 10:10</span></p>
+        <p>In early October of 2024, Jacob moved to Philadelphia, looking for new life and a fresh start. On October 7, he found himself praying using the words of Percy Mayfield, &ldquo;Please send me someone to&nbsp;love.&rdquo;</p>
+        <p>Two months prior, I had also moved to Philly from the small town of Steubenville, Ohio, feeling called to leave behind the last 7 years of familiar comforts, like weekly tap dancing at a local studio and hosting monthly brunches with friends after church. I felt compelled and spoken to by the Lord through the words of Aly Aleigha, &ldquo;Into the unexplored enclave, little bird, take&nbsp;flight.&rdquo;</p>
+        <p>I knew that starting a new life in a new city meant finding a new community. And I knew that I could find community through church, and also through my favorite hobby&nbsp;&mdash; dancing. I started visiting different parishes and searching for West Coast Swing, Salsa, and Lindy Hop dance socials, and soon was invited to attend a Blues and Fusion event on October 18th in Old&nbsp;City.</p>
+    </section>
+
+    <section class="story-section">
+        <h2>The Sidewalk</h2>
+        <p>A block away from the Liberty Bell, standing outside the Concierge Ballroom and waiting for the doors to open, Jacob and I met. He was tall, handsome, and vivacious&nbsp;&mdash; with a kind of golden retriever-energy. The night started with a blues lesson, during which we rotated partners frequently. While I was occupied with learning the style&rsquo;s rhythm and the names of my quickly turning-over partners, Jacob has since admitted that he was stealing glances through the&nbsp;mirror.</p>
+        <p>At the end of the night, as I gathered my belongings, I caught sight of Jacob across the room out of the corner of my eye. I had this sense that he wanted to talk to me, and curious to see where things might go, I paused by the door to fill out an interest form and give him the chance to&nbsp;approach.</p>
+        <p class="story-accent">Bingo.</p>
+        <p>He came over, we chatted a bit more, and then said our goodbyes. After I turned and walked out, he made note of my name from the interest form. The next morning, I saw I had a friend request from Jacob Stephens (sent at 2am the night&nbsp;before).</p>
         
         <div class="photo-carousel">
             <div class="carousel-container">
@@ -24,10 +35,32 @@ include __DIR__ . '/includes/header.php';
             </div>
         </div>
     </section>
-    
+
     <section class="story-section">
-        <h2>Falling Deeper</h2>
-        <p><em>Jacob had been looking into becoming Catholic, and I had been to St. Agatha-St. James Church a couple of times since recently moving to Philly. We started attending events at St. AJs and dances throughout the city, and after a few dates and a spontaneous knock on my door with a TON of halal food and hummus, we started officially&nbsp;dating.</em></p>
+        <h2>Rosie the Riveter&rsquo;s Birthday</h2>
+        <p>A week later, I ran into Jacob again&nbsp;&mdash; this time at a Lindy Hop social at Cherry St Harbor. We danced several songs over the evening before I ran off to welcome friends from out of town, who had come to celebrate my birthday. Jacob intentionally asked for one last song before I left, so that he could be my last dance on my&nbsp;birthday.</p>
+    </section>
+
+    <section class="story-section">
+        <h2>The Orbits Tighten</h2>
+        <p>Another week later, on November 1, the Feast of All Saints, we both found ourselves back at the Concierge Ballroom for a Balboa dance. As we danced and conversed about our days, I mentioned I had gone to Mass for the feast&nbsp;day.</p>
+        <p>Jacob&rsquo;s ears pricked&nbsp;&mdash; he had been feeling drawn to explore the Catholic Church and join a formation class to learn more. At this time I still hadn&rsquo;t settled on a parish community for myself, but had been to St. Agatha St. James a couple of times and thought the church was beautiful and the community faithful. I invited him to join me sometime, hoping I could snag him for the Church and maybe also&nbsp;myself&hellip;</p>
+        <p>The next day, Jacob invited me to join him and a couple other dance friends in seeing live theater&nbsp;&mdash; <em>The Obama Musical</em> (which, if you&rsquo;re wondering, was not very good but <em>was</em> entertaining and at times a bit of a fever dream). Afterwards we got tacos, and somehow ended up on the topic of Pope John Paul II&rsquo;s Theology of the Body while he drove me&nbsp;home.</p>
+        <p>It was at this point that I really started wondering, &ldquo;Who is this man, and where did he come from? How do I make sure I sound as interesting to him as he does to&nbsp;me?&rdquo;</p>
+    </section>
+
+    <section class="story-section">
+        <h2>Falling</h2>
+        <p>Through November, we texted with some regularity, carpooled to dance events, and even cooked in my kitchen together for a dance group&rsquo;s Friendsgiving dinner. I told him I was going to see <em>Hamilton</em> (much better than <em>The Obama Musical</em>) with friends from church, and from my seat sent him a picture of my view of the stage to tease him about not joining me. Apparently that grainy photo from my cheap Android and his growing desire to leave an impression on me were enough for him to find me in the balcony at intermission with a ticket he bought last minute. As the lights flickered and Act II started, I frantically texted my future maid-of-honor, asking for confirmation of if I was living in a Hallmark&nbsp;movie.</p>
+        <p>One Sunday, Jacob joined me for Mass, and I noticed him get emotional before I went up to receive Communion. He was recalling his prayer from October 7, and there in the church next to me felt with certainty that God was answering it. Knowing that he could not yet receive the Eucharist as he was still in formation, I pulled up an Act of Spiritual Communion for him. He read it, and the tears started flowing. He later shared that he was deeply moved by the parts of the prayer mentioning <em>a contrite heart</em> and <em>the poor dwelling my heart offers You</em>. He felt the ache of his old self, and a beckoning towards a newness of life. I myself was grateful for this encounter I could tell he was having with Christ, and wondered what this new path in my life would play out to&nbsp;be.</p>
+    </section>
+
+    <section class="story-section">
+        <h2>The First Date</h2>
+        <p>We finally went on our first official date, only a month after meeting but it felt long overdue. After witnessing me make a sausage dish for Friendsgiving, a day after having made the same for another group dinner, he was convinced&nbsp;&mdash; &ldquo;This girl <em>LOVES</em> sausage&rdquo;&nbsp;&mdash; and found a local, vibey spot known for their sausage sandwiches, hoping to&nbsp;impress.</p>
+        <p class="story-footnote"><em>Note: I like sausage. But the back-to-back dishes were more about the convenience than any preoccupation Jacob assumed I had with the ground&nbsp;pork.</em></p>
+        <p>We went on two more dates, I daringly wrote him half of a poem for his half birthday, and then he went radio silent for three days. So when he finally responded by showing up unannounced on my doorstep with two chicken maroosh sandwiches, a massive tub of hummus, and two kinds of baklava, my brain short-circuited. I apologized for my inability to sit and have dinner with him as I was just about to run out for the evening. But later that night we reconnected, walked through the first snow of the year, and after some prodding&nbsp;&mdash; I drew out his intentions. He envisioned a long journey ahead for&nbsp;us.</p>
+        <p class="story-accent">With wonder and joy, I accepted his invitation.</p>
         
         <div class="photo-carousel carousel-landscape">
             <div class="carousel-container">
